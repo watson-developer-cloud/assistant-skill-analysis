e851ba2847d112b1225456d552d9ad281855df0d
Yang Yu <yangyuphd@gmail.com>
2019-09-18 14:45:32 -0400

    feat(test): add unit tests

---
+++ src/test/python/end2end_test.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+
+sys.path.append('src/main/python')
+from utils import skills_util
+
+class TestNotebook(unittest.TestCase):
+    def setUp(self):
+        CONFIG_FILE = './wa_config.txt'
+        with open(CONFIG_FILE) as fi:
+            self.apikey = fi.readline().strip()
+            self.wksp_id = fi.readline().strip()
+            
+    def test_notebook(self):
+        test_file = 'src/test/resources/test_workspaces/customer_care_skill_test.tsv'
+        nb, errors = skills_util.run_notebook('skill_analysis.ipynb', self.apikey, self.wksp_id, test_file, 'notebook_output')
+        self.assertEqual(errors, [])
+    
+if __name__ == '__main__':
+    unittest.main()
